ee5dedb18bb32e6f6c86e7c75a7e3cd430d2c78e
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2016-02-28 16:12:55 +0100

    Update TicTacToe.ino

--- TicTacToe.ino
+++ TicTacToe.ino
@@ -4,9 +4,9 @@
 const int LEDPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10}; //D2 - D10
 const int ButtonPins[] = {11, 12, 13}; //D11 - D13
                                        //D11 & D12 are for selection, D13 is for confirmation
-int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0); //Set the Values of the LEDs
+int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0); //Set the values of the LEDs
                                                    //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
-bool Player = false //false = P1, true = P2
+bool Player //false = P1, true = P2
 
 void setup()
 {
@@ -21,12 +21,12 @@ void setup()
 }
 
 //The following 2 Functions will be used to "mark" a Player in possession of an LED. Since only unicolour LEDs are being used, the LEDs will just blink in a set interval.
-void LEDMarkP1(int LPos) //Mark P1 in Possession of an LED.+
+void LEDMarkP1(int LPos) //Mark P1 in Possession of an LED.
 {
   digitalWrite(LEDPins[LPos], HIGH); //'LPos' is used to reference which LED in the Array
-  delay(1000);                       //Wait 1 second
+  delay(500);                       //Wait 0.5 seconds
   digitalWrite(LEDPins[LPos], LOW);
-  delay(1000);
+  delay(500);
 }
 
 void LEDMarkP2(int LPos2) //Mark P2 in Possession of an LED.
@@ -37,11 +37,44 @@ void LEDMarkP2(int LPos2) //Mark P2 in Possession of an LED.
   delay(250);
 }
 
-void CheckButton(int Bnum) //Function to get State of specified button
+void LEDMarkSel(int LPos3) //Mark a LED being selected
+{
+  digitalWrite(LEDPins[LPos3], HIGH); //'LPos' is used to reference which LED in the Array
+  delay(1000);                       //Wait 1 second
+  digitalWrite(LEDPins[LPos3], LOW);
+  delay(1000);
+}
+
+void WinAnimation(int Who) //"Animation" to play if either Player has won.
+{
+ if (Who == 1) //If P1 won
+ {
+  
+ }
+ if (Who == 2) //If P2 won
+ {
+  
+ }
+}
+
+void GridCheckP1() //Check LED matrix if P1 got 3 LEDs in a row.
+{
+  if (LEDVals[0] == 1)
+  { if (LEDVals[1] == 1) 
+   { if (LEDVals[2] == 1) 
+    
+   }
+  }
+  //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
+}
+
+void CheckButton(int Bnum) //Function to get state of specified button
 {
   buttonState = digitalRead(ButtonPins[Bnum])
 }
 
 void loop()
 {
+  Player = false //Select P1
+  
 }
