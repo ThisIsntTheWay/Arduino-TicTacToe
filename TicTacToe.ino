d3a7f82625d2385a0451f751adf49efb1d29bfb5
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2016-02-28 17:09:15 +0100

    Update TicTacToe.ino

--- TicTacToe.ino
+++ TicTacToe.ino
@@ -5,7 +5,8 @@ const int LEDPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10}; //D2 - D10
 const int ButtonPins[] = {11, 12, 13}; //D11 - D13
                                        //D11 & D12 are for selection, D13 is for confirmation
 int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Set the values of the LEDs
-                                                   //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
+//0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3), 5 = win (P1), 6 = win (P2)
+
 bool Player; //false = P1, true = P2
 
 void setup()
@@ -47,11 +48,11 @@ void LEDMarkSel(int LPos3) //Mark a LED being selected
 
 void WinAnimation(int Who) //"Animation" to play if either Player has won.
 {
- if (Who == 1) //If P1 won
+ if (Who == 1) //If P1 won, make an "O"
  {
   for (int i=0; i<9; i++)
   {
-    if (LEDVals[i] == 1)
+    if (LEDVals[i] == 5)
     {
       digitalWrite(LEDPins[i], HIGH);
     }
@@ -61,11 +62,11 @@ void WinAnimation(int Who) //"Animation" to play if either Player has won.
     }
   }
  }
- if (Who == 2) //If P2 won
+ if (Who == 2) //If P2 won, make a "X"
  {
   for (int i=0; i<9; i++)
   {
-    if (LEDVals[i] == 3)
+    if (LEDVals[i] == 6)
     {
       digitalWrite(LEDPins[i], HIGH);
     }
@@ -78,12 +79,54 @@ void WinAnimation(int Who) //"Animation" to play if either Player has won.
 }
 
 
-void GridCheckP1() //Check LED matrix if P1 got 3 LEDs in a row.
+void GridCheck(int PCheck) //Check LED matrix if any Player got 3 in a row
 {
-  if (LEDVals[0] == 1)
-  { if (LEDVals[1] == 1) 
-   { if (LEDVals[2] == 1) 
-    
+  if (LEDVals[0] == PCheck) //D2, D3, D4
+  { if (LEDVals[1] == PCheck) 
+   { if (LEDVals[2]  == PCheck) 
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[0] == PCheck) //D2, D5, D8
+  { if (LEDVals[3] == PCheck)
+   { if (LEDVals[6] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[0] == PCheck) //D2, D6, D10
+  { if (LEDVals[4] == PCheck)
+   { if (LEDVals[8] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[1] == PCheck) //D3, D6, D9
+  { if (LEDVals[4] == PCheck)
+   { if (LEDVals[7] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[2] == PCheck) //D4, D7, D10
+  { if (LEDVals[5] == PCheck)
+   { if (LEDVals[8] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[3] == PCheck) //D5, D6, D7
+  { if (LEDVals[4] == PCheck)
+   { if (LEDVals[5] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[6] == PCheck) //D8, D9, D10
+  { if (LEDVals[7] == PCheck)
+   { if (LEDVals[8] == PCheck)
+    WinAnimation(PCheck);
+   }
+  }
+  else if (LEDVals[2] == PCheck) //D4, D6, D8
+  { if (LEDVals[4] == PCheck)
+   { if (LEDVals[6] == PCheck)
+    WinAnimation(PCheck);
    }
   }
   //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
