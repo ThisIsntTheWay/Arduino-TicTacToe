9d314cccbb9df58c30f1cf5357853cc788ae2aeb
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2016-02-28 14:43:30 +0100

    Update TicTacToe.ino

--- TicTacToe.ino
+++ TicTacToe.ino
@@ -4,7 +4,7 @@
 const int LEDPins[] = {2, 3, 4, 5, 6, 7, 8, 9, 10}; //D2 - D10
 const int ButtonPins[] = {11, 12, 13}; //D11 - D13
                                        //D11 & D12 are for selection, D13 is for confirmation
-const int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0); //Set the Values of the LEDs
+int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0); //Set the Values of the LEDs
                                                    //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
 
 void setup()
