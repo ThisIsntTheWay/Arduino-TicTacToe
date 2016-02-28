927150250f1bfd6e09d538c961c9cb47ead777fd
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2016-02-28 19:49:20 +0100

    Update TicTacToe.ino

--- TicTacToe.ino
+++ TicTacToe.ino
@@ -7,8 +7,9 @@ const int ButtonPins[] = {11, 12, 13}; //D11 - D13
                                        //D11 & D12 are for selection, D13 is for confirmation
 int LEDVals[] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Set the values of the LEDs
                                              //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
-int Up = 3;
-int Right = 3;
+int Up = 0;
+int Right = 0;
+int Pos = 0;
 
 bool Player; //false = P1, true = P2
 
@@ -30,7 +31,6 @@ void setup()
  }
 }
 
-/*
 //The following 2 Functions will be used to "mark" a Player in possession of an LED. Since only unicolour LEDs are being used, the LEDs will just blink in a set interval.
 void LEDMarkP1(int LPos) //Mark P1 in Possession of an LED.
 { digitalWrite(LEDPins[LPos], HIGH); //'LPos' is used to reference which LED in the Array
@@ -49,7 +49,6 @@ void LEDMarkSel(int LPos3) //Mark a LED being selected
   delay(1000);                       //Wait 1 second
   digitalWrite(LEDPins[LPos3], LOW);
   delay(1000); }
-*/
 
 void WinAnimation(int Who) //"Animation" to play if either Player has won.
 {
@@ -136,24 +135,44 @@ void GridCheck(int PCheck) //Check LED matrix if any Player got 3 in a row
   //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
 }
 
+void UpPos() {Pos = Up + Right; Serial.printLn("Position of selected LED updated."); }
+
 void SelectLED(int Dir) //Parameter indicates direction. 1 = Up, 2 = Right
 {
- if (Dir == 1)
- {
-  if (Up >= 3)
+  if (Dir == 1)
   {
-    Up = 1;
+    if (Up > 3 ) { Up = 1; }
+    Up++;
+    UpPos();
+    if (Player = false) { LEDVals[Pos] = 2; }
+    else { LEDVals[Pos] = 4; }
   }
   else
   {
-    
+    if (Right > 3 ) { Right = 1; }
+    Right++;
+    UpPos();
+    if (Player = false) { LEDVals[Pos] = 2; }
+    else { LEDVals[Pos] = 4; }
   }
- }
-  //0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P3)
+}
+//0 = off, 1 = on (P1), 2 = sel (P1), 3 = on (P2), 4 = sel (P2)
+
+void SetLED();
+{
+  if (LEDVals[Pos] == 2) { LEDVals[Pos] = 1; }
+  else if (LEDVals[Pos] == 4) {LEDVals[Pos] = 3; }
 }
 
 void loop() //D11 (0) & D12 (1) are for selection, D13 (2) is for confirmation
 {
+ GridCheck();
  Player = false //Select P1
- if (digitalRead(ButtonPins[0] == HIGH) {
+ 
+ if (digitalRead(ButtonPins[0] == HIGH) { SelectLED(1); }
+ if (digitalRead(ButtonPins[1] == HIGH) { SelectLED(2); }
+ if (digitalRead(ButtonPins[2] == HIGH) { SetLED(); }
+ 
+ for (int i=0; i<9; i++) //Set all LEDs as OUTPUT and turn them off, assuming they are turned on.
+ {
 }
