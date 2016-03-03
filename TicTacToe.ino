ce31739ba9c755cf26e72a5770ea93c340b8bd0c
ThisIsntTheWay <ThisIsntTheWay@users.noreply.github.com>
2016-03-03 11:24:33 +0100

    Update TicTacToe.ino

--- TicTacToe.ino
+++ TicTacToe.ino
@@ -35,27 +35,6 @@ void setup()
   Serial.println("-------------------[END ]-------------------");
 }
 
-/*
-//The following 2 Functions will be used to "mark" a Player in possession of an LED. Since only unicolour LEDs are being used, the LEDs will just blink in a set interval.
-void LEDMarkP1(int LPos) //Mark P1 in Possession of an LED.
-{ digitalWrite(LEDPins[LPos], HIGH); //'LPos' is used to reference which LED in the Array
-  delay(500);                        //Wait 0.5 seconds
-  digitalWrite(LEDPins[LPos], LOW);
-  delay(500); }
-
-void LEDMarkP2(int LPos2) //Mark P2 in Possession of an LED.
-{ digitalWrite(LEDPins[LPos2], HIGH); //'LPos' is used to reference which LED in the Array
-  delay(250);                         //Wait 0.25 seconds
-  digitalWrite(LEDPins[LPos2], LOW);
-  delay(250); }
-
-void LEDMarkSel(int LPos3) //Mark a LED being selected
-{ digitalWrite(LEDPins[LPos3], HIGH); //'LPos' is used to reference which LED in the Array
-  delay(1000);                       //Wait 1 second
-  digitalWrite(LEDPins[LPos3], LOW);
-  delay(1000); }
-*/
-
 void WinAnimation(int Who) //"Animation" to play if either Player has won.
 {
  if (Who == 1) //If P1 won, make an "O"
@@ -222,7 +201,7 @@ void loop() //D11 (0) & D12 (1) are for selection, D13 (2) is for confirmation
   }*/
   if (LEDVals[i] == 1) 
   {
-   digitalWrite(LEDPins[i], HIGH); //'LPos' is used to reference which LED in the Array
+   digitalWrite(LEDPins[i], HIGH);
    delay(500);                     //Wait 0.5 seconds
    digitalWrite(LEDPins[i], LOW);
    delay(500);
@@ -232,7 +211,7 @@ void loop() //D11 (0) & D12 (1) are for selection, D13 (2) is for confirmation
   }
   else if (LEDVals[i] == 3) 
   {
-   digitalWrite(LEDPins[i], HIGH); //'LPos' is used to reference which LED in the Array
+   digitalWrite(LEDPins[i], HIGH);
    delay(250);                     //Wait 0.25 seconds
    digitalWrite(LEDPins[i], LOW);
    delay(250);
@@ -242,7 +221,7 @@ void loop() //D11 (0) & D12 (1) are for selection, D13 (2) is for confirmation
   }
   else if (LEDVals[i] == 2)
   {
-   digitalWrite(LEDPins[i], HIGH); //'LPos' is used to reference which LED in the Array
+   digitalWrite(LEDPins[i], HIGH);
    delay(1000);                    //Wait 1 second
    digitalWrite(LEDPins[i], LOW);
    delay(1000);
@@ -252,7 +231,7 @@ void loop() //D11 (0) & D12 (1) are for selection, D13 (2) is for confirmation
   }
   else if (LEDVals[i] == 4)
   {
-   digitalWrite(LEDPins[i], HIGH); //'LPos' is used to reference which LED in the Array
+   digitalWrite(LEDPins[i], HIGH);
    delay(1000);                    //Wait 1 second
    digitalWrite(LEDPins[i], LOW);
    delay(1000);
